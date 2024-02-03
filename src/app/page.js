94343cc2781168c441bee66bdb958d59bb11c0c1
Aditya Sijaria <adi@gmail.com>
2024-02-04 03:29:18 +0530

    About start

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Navbar from "@/components/Navbar";
 import Image from "next/image";
+import About from "@/components/About";
 export default function Home() {
   return (
     <main className="">
       <Navbar/>
+      <About/>
     </main>
   );
 }
