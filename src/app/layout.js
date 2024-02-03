b5c9648776b37d30be30f9c682f39045434ba2f1
Dhananjay Sarathe <dhananjay.s@quickads.ai>
2024-02-04 02:15:34 +0530

    navbar in process

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <head>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.css" rel="stylesheet" />
+    </head>
+      <body className={inter.className}>
+        {children}
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.js"></script>
+        {/* <script src="../node_modules/flowbite/dist/flowbite.min.js"></script> */}
+      </body>
     </html>
   );
 }
