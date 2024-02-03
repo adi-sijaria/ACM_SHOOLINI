a7fd0e943d5421996726469ab0542f8eae4af117
Aditya Sijaria <adi@gmail.com>
2024-02-04 02:51:43 +0530

    photos name added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,7 +26,7 @@ const Navbar = () => {
           aria-expanded="false"
         >
           <span class="sr-only">Open main menu</span>
-          {/* <svg
+          <svg
             class="w-5 h-5"
             aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
@@ -40,7 +40,7 @@ const Navbar = () => {
               stroke-width="2"
               d="M1 1h15M1 7h15M1 13h15"
             />
-          </svg> */}
+          </svg>
         </button>
         <div class="hidden w-full md:block md:w-auto" id="navbar-default">
           <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
